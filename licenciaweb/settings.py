9322ae80ed3dc58336d4e4925c9426b725b9a399
MalkavianxX <mot46000@gmail.com>
2023-12-03 01:59:14 -0600

    csrf

--- licenciaweb/settings.py
+++ licenciaweb/settings.py
@@ -121,6 +121,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+CSRF_TRUSTED_ORIGINS = ['https://licenapp.cloud/',]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
