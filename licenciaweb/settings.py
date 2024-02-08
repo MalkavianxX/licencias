893c23dcbcde8b3a9f7731ea327d13668a4016f7
MalkavianxX <mot46000@gmail.com>
2024-02-08 17:03:55 -0600

    jj

--- licenciaweb/settings.py
+++ licenciaweb/settings.py
@@ -121,7 +121,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-CSRF_TRUSTED_ORIGINS = ['http://licenapp.cloud', 'https://licenapp.cloud','https://licencias-bajacalifornia-gob.com','http://licencias-bajacalifornia-gob.com/']
+CSRF_TRUSTED_ORIGINS = ['http://licenapp.cloud', 'https://licenapp.cloud','https://licencias-bajacalifornia-gob.com','http://licencias-bajacalifornia-gob.com',]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
